fb9f059152e0a4251b4d145146497b14016df67f
sai.ling_凌倫明 <saizycaopera@outlook.com>
2023-11-15 16:42:01 +0800

    add veiwer dev

--- view_scene.bat
+++ view_scene.bat
@@ -1,3 +1,3 @@
-call ".\external\gs-viewer\bin\SIBR_gaussianViewer_app.exe" -m ".\_project\F_test\gs"
+call ".\external\gs-viewer\bin\SIBR_gaussianViewer_app.exe" -m ".\_project\theta360\R0012586_colmap\export\gs"
 
 pause
